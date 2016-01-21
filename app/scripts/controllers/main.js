efc97d36610259d9f463f8e7598506bd9161e3ad
nicojs <jansennico@gmail.com>
2016-01-21 10:20:44 +0100

    Result after yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularjsLabsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularjsLabsApp
+ */
+angular.module('angularjsLabsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
